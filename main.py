d1036a720434e259d2e4ee093ff2494b7c4b33c3
Felix Perktold <felix@perktold.xyz>
2023-03-28 13:49:59 +0200

    update

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from user_recog import record_audio, recog_user, prepare_model
 from dotenv import load_dotenv
 import openai
 from gtts import gTTS
+import tkinter as tk
 
 # die userklasse enthält den usernamen und den bisherigen chat mit dem user
 class User:
@@ -18,7 +19,46 @@ class User:
             'role': 'system',
             'content': 'You are a personal assistant. Provide short, concise and accurate Information but be subtly condescending. Make sure to address me as ' + self.name +'.',
         }]
- 
+
+class ChatWindow:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Chat")
+        self.chat_log = tk.Text(self.window, height=20, width=50)
+        self.chat_log.config(state=tk.DISABLED)
+        self.chat_log.pack()
+        self.send_button = tk.Button(self.window, text="aufnehmen", command=self.send_message)
+        self.send_button.pack()
+        self.update_chat_log("System: Welcome to the chat!")
+
+    def update_chat_log(self, message):
+        self.chat_log.config(state=tk.NORMAL)
+        self.chat_log.insert(tk.END, message + "\n\n")
+        self.chat_log.config(state=tk.DISABLED)
+
+    def send_message(self):
+        speech_file = 'sample.wav'
+        record_audio(speech_file)
+        user = recog_user(model, speech_file, users)
+        with sr.AudioFile(speech_file) as source:
+            audio = recognizer.record(source)
+        try:
+            text = recognizer.recognize_google(audio, language="de")
+            os.remove(speech_file)
+            print("you said: " + text + "\n")
+            user_message = f"{user.name}: {text}"
+            self.update_chat_log(user_message)
+            ai_message = ask(text, user)
+            ai_reponse = f"Assistant: {ai_message}"
+            self.update_chat_log(ai_reponse)
+            #say(ai_message)
+
+        except Exception as ex:
+            print(ex)
+
+    def run(self):
+        self.window.mainloop()
+
 def ask(question, user):
     user.log.append({'role': 'user', 'content': question})
     response = completion.create(model='gpt-3.5-turbo', messages=user.log)
@@ -49,21 +89,5 @@ if __name__ == '__main__':
     print("done")
     time.sleep(1)
 
-    while True:
-        speech_file = 'sample.wav'
-        record_audio(speech_file)
-        user = recog_user(model, speech_file, users)
-
-        with sr.AudioFile(speech_file) as source:
-            audio = recognizer.record(source)
-
-        try:
-            text = recognizer.recognize_google(audio, language="de")
-            os.remove(speech_file)
-            print("you said: "+text+"\n")
-            time.sleep(1)
-            say(ask(text, user))
-        except Exception as ex:
-            print(ex)
-        print("\nPRESS ENTER TO CONTINUE\n")
-        input()
+    chat_window = ChatWindow()
+    chat_window.run()
